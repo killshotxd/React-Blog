370f6ba28a22cd57807c6fb53c20bb59c9fccf36
killshotxd <hassanansari211@gmail.com>
2022-08-30 10:37:56 +0530

    Route setup for editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./Home";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Editor from "./components/Editor/Editor";
 const App = () => {
   return (
     <ChakraProvider>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<Editor />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import styles from "./Home.module.css";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import Body from "./components/Body/Body";
-
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className={styles.header}>
         <div className={styles.leftPart}>
           <ul>Blog</ul>
-          <ul>New Post</ul>
+          <ul
+            onClick={() => navigate("/edit")}
+            style={{ cursor: "pointer", textDecorationLine: "underline" }}
+          >
+            New Post
+          </ul>
           <ul>List</ul>
         </div>
         <div className={styles.rightPart}>
