208d1eb5b3cc3315d59b479bdf546f640c667e66
killshotxd <hassanansari211@gmail.com>
2022-08-30 10:42:11 +0530

    Delete BTN

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Body.module.css";
-import { Image, Box, Badge, SimpleGrid } from "@chakra-ui/react";
+import { Image, Box, Badge, SimpleGrid, Button } from "@chakra-ui/react";
 const Body = () => {
   const property = {
     imageUrl: "https://bit.ly/2Z4KKcF",
@@ -19,32 +19,9 @@ const Body = () => {
         {/* Card Start */}
         <div className={styles.card}>
           <div className={styles.cardImg}>
-            <Box
-              boxSize="xs"
-              height="auto"
-              objectFit="cover"
-              display="flex"
-              justifyContent="center"
-            >
-              <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
-            </Box>
-          </div>
-          <div className={styles.cardBadge}>
-            <Badge colorScheme="purple">Default</Badge>
-          </div>
-          <div className={styles.cardContent}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              aperiam ullam reiciendis commodi incidunt, quod voluptatibus
-              facilis dicta? Qui hic officia ullam unde recusandae, eveniet .
-            </p>
-          </div>
-        </div>
-        {/* ---------Card End------------- */}
-
-        {/* Card Start */}
-        <div className={styles.card}>
-          <div className={styles.cardImg}>
+            <div className={styles.deleteBtn}>
+              <Button>Delete</Button>
+            </div>
             <Box
               boxSize="xs"
               height="auto"
