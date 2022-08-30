07f5e1c53602ee9970108537e20c2e7eb0f55c29
killshotxd <hassanansari211@gmail.com>
2022-08-30 14:53:52 +0530

    Editor Page Ready

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,12 +1,61 @@
 import React from "react";
-
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
 import Header from "../Header/Header";
 import styles from "./Editor.module.css";
 const Editor = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className={styles.masterCont}>
+        <div className={styles.heading}>Create a Blog</div>
+        <div className={styles.container}>
+          <div className={styles.containerContent}>
+            <div className={styles.title}>
+              <FormControl>
+                <FormLabel>Title</FormLabel>
+                <Input
+                  isRequired
+                  type="text"
+                  placeholder="Enter Title for your Blog"
+                  width="40vw"
+                />
+              </FormControl>
+            </div>
+            <div className={styles.title}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  isRequired
+                  type="text"
+                  placeholder="Enter Name for your Blog"
+                  width="40vw"
+                />
+              </FormControl>
+            </div>
+
+            <div className={styles.title}>
+              <FormLabel>Content</FormLabel>
+              <Textarea
+                width="40vw"
+                isRequired
+                type="text"
+                placeholder="Enter Content for blog."
+              />
+            </div>
+
+            <Button colorScheme="teal">Create</Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
